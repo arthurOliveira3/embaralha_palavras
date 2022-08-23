f0d88d80b100bd069ed70a377ef2d05b529400da
Raj IFC <ruhtra.oliveira3@gmail.com>
2022-08-23 20:19:05 -0300

    Finished the basic demmands of the project.

--- embaralhaPalavras.py
+++ embaralhaPalavras.py
@@ -6,17 +6,33 @@ def embaralhaPalavras(palavra):
     palavraEmbaralhada = ''.join(lista)
     return palavraEmbaralhada
 
+def palavras_de_animo():
+    palavras = ["tente novamente!", "você consegue!", "na próxima você vai conseguir!", "não desista!", "persista!"]
+    random.shuffle(palavras)
+    return palavras
+
+
 palavra = "masqueico"
 embaralhada = embaralhaPalavras(palavra)
+palavra_animo = palavras_de_animo()
+index = 0
 
 for item in range(5):
-    print(f"\nVocê tem {5 - item} tentativas")
-    print(f"\nA palavra é:\n{embaralhada}")
+    print(f"\n\nVocê tem {5 - item} tentativas!")
+    print(f"\nA palavra é: {embaralhada}")
+
     tentativa = input("\nDigite a palavra que você acha certa: ")
     tentativa = tentativa.lower()
-    if len(tentativa) == len(palavra) and tentativa == palavra:
-
+    
+    if tentativa == palavra:
         print("PARABÉNS!!!")
         break
-
-print(f"A palavra certa é: {palavra}")
+    elif len(tentativa) > len(embaralhada):
+        print("A sua tentativa tem mais letras do que o permitido")
+    elif  len(tentativa) < len(embaralhada):
+        print("A sua tentativa tem menos letras do que o permitido")
+    elif item != 4:
+        print(f"\nVocê errou, mas {palavra_animo[index]}")
+    else:
+        print(f"\nVocê perdeu \nA palavra certa é: {palavra}")
+    index +=1
